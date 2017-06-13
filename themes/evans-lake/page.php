d4d6b731514a60ab7615fb8977e0b74ab62f765d
Victor Salmon <vasalmon@hotmail.com>
2017-06-13 14:18:55 -0700

    add working functions to extras.php to make secondary nav submenu on all pages

--- themes/evans-lake/page.php
+++ themes/evans-lake/page.php
@@ -7,30 +7,20 @@
 
 get_header(); ?>
 
-
-	<div id="primary" class="content-area">
-
-		<nav id="sub-navigation" class="sub-navigation">
-			<button class="menu-toggle" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
-			<?php wp_nav_menu( array( 
-				'theme_location' => 'primary', 
-				'menu_id' => 'primary-menu',
-				'submenu' => get_the_title($post->post_parent)
-				) ); ?>
-		</nav><!-- #site-navigation -->
-
-		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
-
-	</div><!-- #primary -->
-
-
 <?php get_sidebar(); ?>
+
+<div id="primary" class="content-area">
+
+	<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+		<?php endwhile; // End of the loop. ?>
+
+	</main><!-- #main -->
+
+</div><!-- #primary -->
+
 <?php get_footer(); ?>
--- themes/evans-lake/sidebar.php
+++ themes/evans-lake/sidebar.php
@@ -12,5 +12,13 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 
 <div id="secondary" class="widget-area" role="complementary">
 	<?php	dynamic_sidebar( 'sidebar-1' ); ?>
+	<nav id="sub-navigation" class="sub-navigation">
+		<button class="menu-toggle" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
+		<?php wp_nav_menu( array( 
+			'theme_location' => 'primary', 
+			'menu_id' => 'primary-menu',
+			'submenu' => get_the_title($post->post_parent)
+		) ); ?>
+	</nav><!-- #site-navigation -->
 
 </div><!-- #secondary -->
