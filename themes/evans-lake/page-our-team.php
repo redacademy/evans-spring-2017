bf00b7bf702e2b2e524dbd861bdbf348f7fd862d
Victor Salmon <vasalmon@hotmail.com>
2017-06-14 15:18:06 -0700

    Add page.php default to page-our-team.php custom page template

--- themes/evans-lake/page-our-team.php
+++ themes/evans-lake/page-our-team.php
@@ -1,1 +1,33 @@
-<p>page-our-team.php</p>
+<?php
+/**
+ * The template for displaying the Our Team page.
+ *
+ * @package Evans_Lake_Theme
+ */
+
+get_header(); 
+get_sidebar(); ?>
+
+<div class="hero-image"><?php the_post_thumbnail( 'full' ); ?></div>
+<div id="primary" class="content-area container">
+	<div class="sub-navigation">
+		<?php wp_nav_menu( array( 
+			'theme_location' => 'primary', 
+			'menu_id' => 'primary-menu',
+			'submenu' => get_the_title($post->post_parent)
+		) ); ?>
+	</div>
+
+	<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+		<?php endwhile; // End of the loop. ?>
+
+	</main><!-- #main -->
+
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
