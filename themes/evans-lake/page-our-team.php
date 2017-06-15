54c3dfb0f9b0a262ee0566bacf97a4a4a4c1a9fe
Victor Salmon <vasalmon@hotmail.com>
2017-06-15 16:30:12 -0700

    Delete full time staff content part and move into page-our-team

--- themes/evans-lake/page-our-team.php
+++ themes/evans-lake/page-our-team.php
@@ -31,31 +31,64 @@ get_header(); ?>
 			'posts_per_page'=> 50
 			);
 		$all_staff = new WP_Query($args);
-		// Create storage arrays
-		// $full_time = [];
-		// $summer = [];
-		// $director = [];
-		// $executive = [];
 	?>
-	
-		<!--Populate arrays-->
-		<?php foreach ($all_staff->posts as $staff_member): 
-		?>
+
+	<!--Populate arrays-->
+	<?php foreach ($all_staff->posts as $staff_member): ?>
 		<div>
-						<?php get_template_part( 'template-parts/content', 'staff-fulltime' ); ?>
+			<div class="all-staff">
+				<section class="staff-member">
+					<span class="id">
+						<?php echo $staff_member->ID; ?>
+					</span>
+					<span class="type">
+						<?php
+							$type = CFS()->get( 's_type',  $staff_member->ID ); 
+							print_r($type)[0];
+						?>
+					</span>
+					<br><br>
+					<span class="role">
+						<?php echo CFS()->get( 's_role',  $staff_member->ID,  array( 'format' => 'raw' ) ); ?>
+					</span>
+					<span class="image">
+						<?php echo CFS()->get( 's_img',  $staff_member->ID ); ?>
+					</span>
+					
+					<span class="email">
+						<?php echo CFS()->get( 's_email',  $staff_member->ID ); ?>
+					</span>
+					<span class="tel-1">
+						<?php echo CFS()->get( 's_tel_1',  $staff_member->ID ); ?>
+					</span>
+					<span class="tel-1-is-mobile">
+						<?php echo CFS()->get( 's_tel_1_is_mobile',  $staff_member->ID ); ?>
+					</span>
+					<span class="tel-2">
+						<?php echo CFS()->get( 's_tel_2',  $staff_member->ID ); ?>
+					</span>
+					<span class="tel-2-is-mobile">
+						<?php echo CFS()->get( 's_tel_2_is_mobile',  $staff_member->ID ); ?>
+					</span>
+					<span class="year-started">
+						<?php echo CFS()->get( 's_year_started',  $staff_member->ID ); ?>
+					</span>
+					<span class="bio">
+						<?php echo CFS()->get( 's_bio',  $staff_member->ID ); ?>
+					</span>
+				</section>
+			</div>
 		</div>
-		<?php endforeach; ?>		
+	<?php endforeach; ?>		
 
 	<main id="main" class="site-main" role="main">
-	
-
-<!--board of directors loop-->
+<!---->
+<!--board of directors/executives-->
+<!--board of directors/directors loop-->
 
 <!--Summer Camp staff loop-->
-
-
-
 	</main><!-- #main -->
+
 </div><!-- #primary -->
 <?php wp_reset_query(); ?>
 <?php get_footer(); ?>
