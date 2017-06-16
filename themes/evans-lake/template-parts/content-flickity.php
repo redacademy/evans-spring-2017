62503d23c7b7a7d54a5e78244a80f64cfcb9ad34
Stanislav Maksarov <maksarov.s@gmail.com>
2017-06-16 11:05:48 -0700

    Add minor fixes

--- themes/evans-lake/template-parts/content-flickity.php
+++ themes/evans-lake/template-parts/content-flickity.php
@@ -7,9 +7,16 @@
 
 ?>
 
-<h2 class="activities">Activities</h2>
-<div class="main-carousel" data-flickity='{ "cellAlign": "left", "contain": true }'>
-  <div class="carousel-cell">Bla</div>
-  <div class="carousel-cell">...</div>
-  <div class="carousel-cell">...</div>
+<h2 class="carousel-title">Activities</h2>
+<div class="activities-carousel" data-flickity='{ "cellAlign": "left", "contain": true }'>
+  <div class="activities-cell">Bla</div>
+  <div class="activities-cell">...</div>
+  <div class="activities-cell">...</div>
+</div>
+
+<h2 class="carousel-title">Events</h2>
+<div class="events-carousel" data-flickity='{ "cellAlign": "left", "contain": true }'>
+  <div class="events-cell">Bla</div>
+  <div class="events-cell">...</div>
+  <div class="events-cell">...</div>
 </div>
