2cbbea9f39485ebf35b7af6da0ff90fc0fb582f2
vf <l_vafi@yahoo.com>
2017-06-16 09:55:26 -0700

    Remove faq custom post type

---
+++ themes/evans-lake/page-faq-questions.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package Evans_Lake_Theme
+ */
+
+get_header();  ?>
+
+<div class="hero-image"><?php the_post_thumbnail( 'full' ); ?></div>
+<div id="primary" class="content-area container">
+	<div class="sub-navigation">
+		<?php wp_nav_menu( array( 
+			'theme_location' => 'primary', 
+			'menu_id' => 'primary-menu',
+			'submenu' => get_the_title($post->post_parent)
+		) ); ?>
+	</div>
+
+	<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php the_title(); ?>
+      <?php 
+      $FAQs = CFS()->get('faq');
+      foreach ( $FAQs as $FAQ) { ?>
+      <?php echo $FAQ['faq_question']; ?>
+      <?php echo $FAQ['faq_answer']; ?>
+      <?php } ?>
+      
+
+
+		<?php endwhile; // End of the loop. ?>
+
+	</main><!-- #main -->
+
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
