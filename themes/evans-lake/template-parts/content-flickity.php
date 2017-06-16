e985c07065e0b4d1285bb0018d27b114ed05cd88
Stanislav Maksarov <maksarov.s@gmail.com>
2017-06-16 00:20:51 -0700

    Create content-flickity.php in template parts

---
+++ themes/evans-lake/template-parts/content-flickity.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template part for displaying a message that posts cannot be found.
+ *
+ * @package Evans_Lake_Theme
+ */
+
+?>
+
+<h2 class="activities">Activities</h2>
+<div class="main-carousel" data-flickity='{ "cellAlign": "left", "contain": true }'>
+  <div class="carousel-cell">Bla</div>
+  <div class="carousel-cell">...</div>
+  <div class="carousel-cell">...</div>
+</div>
