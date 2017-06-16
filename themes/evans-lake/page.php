90d640e55c6a9825889ed8aa1ad782c59a39fd6e
Stanislav Maksarov <maksarov.s@gmail.com>
2017-06-16 00:22:12 -0700

    Change thumbnail picture to be used as background image

--- themes/evans-lake/page.php
+++ themes/evans-lake/page.php
@@ -8,7 +8,12 @@
 get_header(); 
 get_sidebar(); ?>
 
-<div class="hero-image"><?php the_post_thumbnail( 'full' ); ?></div>
+<!--<div class="hero-image"><?php the_post_thumbnail( 'full' ); ?></div>-->
+<div class="hero">
+<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
+	<div class="hero-image" style="background-image: linear-gradient(to bottom, rgba(0, 0, 0, .35) 0%, rgba(0, 0, 0, .35) 100%), url('<?php echo $thumb['0'];?>'); background-size: cover,cover; background-position: center, center;">
+	</div>
+</div>
 <div id="primary" class="content-area container">
 	<div class="sub-navigation">
 		<?php wp_nav_menu( array( 
