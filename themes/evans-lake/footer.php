40519b75a40baecffb093c31e9c64f9ca0307283
vf <l_vafi@yahoo.com>
2017-06-19 11:46:49 -0700

    Add footer links

--- themes/evans-lake/footer.php
+++ themes/evans-lake/footer.php
@@ -10,6 +10,27 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="social-media">
+					<h3>Contact Info</h3>
+					<p><i class="fa fa-facebook-official" aria-hidden="true" ></i>
+						<i class="fa fa-twitter-square" aria-hidden="true"></i>
+						<i class="fa fa-google-plus-square" aria-hidden="true"></i>
+					</p>
+				</div>
+        <div class="contact-us">
+					<ul>
+						<li><a href="#">Contact Us</a></li>
+            <li><a href="#">Camp Programs</a></li>
+						<li><a href="#">Donation</a></li>
+					</ul>
+					<ul>
+						<li><a href="#">Maps & Directions</a></li>
+            <li><a href="#">Outdoor Education</a></li>
+						<li><a href="#">job opportunities</a></li>
+					</ul>		
+				</div>	
+				<hr>
+
 				<div class="site-info">
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
