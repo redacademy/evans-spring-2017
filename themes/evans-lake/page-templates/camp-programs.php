a6f631a636a6fbfa715912d23f69155310d8270a
Victor Salmon <vasalmon@hotmail.com>
2017-06-19 19:27:19 -0700

    Add button and breadcrumbs to camp-programs

--- themes/evans-lake/page-templates/camp-programs.php
+++ themes/evans-lake/page-templates/camp-programs.php
@@ -10,9 +10,12 @@ get_header();
 get_sidebar(); ?>
 
 <div class="hero">
-<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
+	<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
 	<div class="hero-image" style="background-image: linear-gradient(to bottom, rgba(0, 0, 0, .35) 0%, rgba(0, 0, 0, .35) 100%), url('<?php echo $thumb['0'];?>'); background-size: cover,cover; background-position: center, center;">
 	</div>
+	<?php the_title( '<h1 class="hero-title">', '</h1>' ); ?>
+
+
 </div>
 <div id="primary" class="content-area container">
 	<div class="sub-navigation">
@@ -24,32 +27,32 @@ get_sidebar(); ?>
 	</div>
 
 	<main id="main" class="site-main" role="main">
-  <button class="orange-button">Register Now</button>
-  <?php evans_lake_breadcrumbs(); ?>
+		<button class="orange-button">Register Now</button>
+		<?php 
+			evans_lake_breadcrumbs(); 
+		?>
 
-<!--from page: camp programs 2017-->
-<!--Description-->
+		<header class="entry-header">
+			<h2><?php echo the_title(); ?></h2>
+		</header><!-- .entry-header -->
 
-<!--Taxonomy titles and scriptions-->
-<?php 
-  $term_ids = get_the_terms ( $taxonomy );
-  echo term_description( $term_id, $taxonomy );  
-?>
+		<div class="entry-content">
+			<?php the_content(); ?>
+		</div><!-- .entry-content -->
+			
+		<!--from page: camp programs 2017-->
+		<!--Description-->
+
+		<!--Taxonomy titles and scriptions-->
+		<?php 
+			$term_ids = get_the_terms ( $taxonomy );
+			echo term_description( $term_id, $taxonomy );  
+		?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
-
-    <header class="entry-header">
-      <?php the_title( '<h1 class="hero-title">', '</h1>' ); ?>
-    </header><!-- .entry-header -->
-
-    <div class="entry-content">
-      <?php the_content(); ?>
-    </div><!-- .entry-content -->
-
 		<?php endwhile; // End of the loop. ?>
 
 	</main><!-- #main -->
-
 </div><!-- #primary -->
 
 <?php get_footer(); ?>
