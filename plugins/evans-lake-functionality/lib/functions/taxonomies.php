221b26c89d3b3054e25f3b42cb1ece9e6a367744
Victor Salmon <vasalmon@hotmail.com>
2017-06-19 18:03:21 -0700

    Create Camp Programs custom post type

--- plugins/evans-lake-functionality/lib/functions/taxonomies.php
+++ plugins/evans-lake-functionality/lib/functions/taxonomies.php
@@ -5,7 +5,6 @@
  * @link  http://codex.wordpress.org/Function_Reference/register_taxonomy
  */
 
-// Add your custom taxonomies here...
 // Register Custom Taxonomy
 function register_staff_type_taxonomy() {
   $name_up_plur = 'Staff Types';
@@ -94,3 +93,48 @@ function register_activity_type_taxonomy() {
 
 }
 add_action( 'init', 'register_activity_type_taxonomy', 0 );
+
+function register_registration_period_type_taxonomy() {
+  $name_up_plur = 'Registration Period Types';
+  $name_up_sing = 'Registration Period Type';
+  $name_low_plur = 'registration period types';
+  $name_low_sing = 'registration period type';
+  $name_low_reg = 'registrationperiodtype';
+	$name_custom_post_type = 'registrationperiod';
+
+
+	$labels = array(
+		'name'                       => $name_up_plur,
+		'singular_name'              => $name_up_sing,
+		'menu_name'                  => $name_up_sing,
+		'all_items'                  => 'All ' . $name_up_plur,
+		'parent_item'                => 'Parent ' . $name_up_sing,
+		'parent_item_colon'          => 'Parent ' . $name_up_sing . ':',
+		'new_item_name'              => 'New ' . $name_up_sing . 'Type Name',
+		'add_new_item'               => 'Add New ' . $name_up_sing,
+		'edit_item'                  => 'Edit ' . $name_up_sing,
+		'update_item'                => 'Update ' . $name_up_sing,
+		'view_item'                  => 'View ' . $name_up_sing,
+		'separate_items_with_commas' => 'Separate ' . $name_low_plur . ' with commas',
+		'add_or_remove_items'        => 'Add or remove ' . $name_low_plur,
+		'choose_from_most_used'      => 'Choose from the most used',
+		'popular_items'              => 'Popular ' . $name_up_plur,
+		'search_items'               => 'Search '. $name_up_plur,
+		'not_found'                  => 'Not Found',
+		'no_terms'                   => 'No ' . $name_low_plur,
+		'items_list'                 => $name_up_plur . 'list',
+		'items_list_navigation'      => $name_up_plur . 'list navigation',
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( $name_low_reg, array( $name_custom_post_type ), $args );
+
+}
+add_action( 'init', 'register_registration_period_type_taxonomy', 0 );
