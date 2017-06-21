fd454ffb23ecb7111f425f13a13621bfbd7b317e
Victor Salmon <vasalmon@hotmail.com>
2017-06-21 16:06:38 -0700

    Complete Summer and Winter seasonal registration tables

--- themes/evans-lake/page-templates/camp-programs.php
+++ themes/evans-lake/page-templates/camp-programs.php
@@ -15,9 +15,8 @@ get_sidebar(); ?>
 	<div class="hero-image" style="background-image: linear-gradient(to bottom, rgba(0, 0, 0, .35) 0%, rgba(0, 0, 0, .35) 100%), url('<?php echo $thumb['0'];?>'); background-size: cover,cover; background-position: center, center;">
 	</div>
 	<?php the_title( '<h1 class="hero-title">', '</h1>' ); ?>
-
-
 </div>
+
 <div id="primary" class="content-area container">
 			<?php // Build submenu on left pane of content area ?>
 	<div class="sub-navigation">
@@ -35,12 +34,7 @@ get_sidebar(); ?>
 		<?php evans_lake_breadcrumbs(); ?>
 
 		<header class="intro-header entry-header">
-			<h2>
-				<?php 
-					echo get_the_title() . ' ';
-					echo CFS()->get( 'camp_year', $post->ID ); 
-				?>
-			</h2>
+			<h2><?php echo get_the_title(); ?></h2>
 		</header> <!--.intro-header-->
 
 		<div class="intro-content entry-content">
@@ -100,25 +94,29 @@ get_sidebar(); ?>
 		<!--Season 1 Table-->
 		<h2><?php echo $season_uc; ?> Registration</h2>
 		<p class="gst">*GST Additional</p>
-		<div class="<?php echo $season; ?>-registration-table">
+		<div class="<?php echo $season; ?> registration-table">
 			<div class="<?php echo $season; ?> table-container">
 				<div class="<?php echo $season; ?> table-header">
-					<span>Camp #</span>
-					<span>Dates</span>
-					<span>Length</span>
-					<span>Camp Program</span>
-					<span>Fee</span>						
+					<span class="camp-number">Camp#</span>
+					<span class="date-range">Dates</span>
+					<span class="length">Length</span>
+					<span class="camp-program">Camp Program</span>
+					<span class="fee">Fee</span>						
 				</div>
 				<?php $reg_sessions = CFS()->get('reg_session_loop'); ?>
 
 				<?php	foreach ($reg_sessions as $reg_session) : ?>
-					<div class="<?php echo $season; ?> table-row">
-						<span class="camp-number"><?php echo $reg_session['reg_camp_no'];?></span>
-						<span class="date-range"><?php echo $reg_session['reg_date_range'];?></span>
-						<span class="length"><?php echo $reg_session['reg_length'];?></span>
-						<span class="camp-program"><?php echo $reg_session['reg_program'];?></span>
-						<span class="fee"><?php echo $reg_session['reg_fee'];?></span>						
-					</div>
+					<?php	if ( 
+							($reg_session['reg_summer_session'] && $season == "summer") ||
+							(!$reg_session['reg_summer_session'] && $season =="winter") ) : ?>
+						<div class="<?php echo $season; ?> table-row">
+							<span class="camp-number"><?php echo $reg_session['reg_camp_no'];?></span>
+							<span class="date-range"><?php echo $reg_session['reg_date_range'];?></span>
+							<span class="length"><?php echo $reg_session['reg_length'];?></span>
+							<span class="camp-program"><?php echo $reg_session['reg_program'];?></span>
+							<span class="fee"><?php echo $reg_session['reg_fee'];?></span>						
+						</div>
+					<?php endif; ?>
 				<?php endforeach; ?>
 
 				<div class="<?php echo $season; ?> table-footer">
@@ -132,6 +130,53 @@ get_sidebar(); ?>
 
 		<button class="orange-button reg-now">Register Now</button>
 
+		<?php 
+			// Set Season 2 Inversely to Season 1
+			if ( CFS()->get( 'summer_first' ) ) {
+				$season = 'winter';
+				$season_uc = 'Winter';
+			}	else {
+				$season = 'summer';
+				$season_uc = 'Summer';
+
+			}
+		?>
+		<!--Season 2 Table-->
+		<h2><?php echo $season_uc; ?> Registration</h2>
+		<p class="gst">*GST Additional</p>
+		<div class="<?php echo $season; ?> registration-table">
+			<div class="<?php echo $season; ?> table-container">
+				<div class="<?php echo $season; ?> table-header">
+					<span class="camp-number">Camp#</span>
+					<span class="date-range">Dates</span>
+					<span class="length">Length</span>
+					<span class="camp-program">Camp Program</span>
+					<span class="fee">Fee</span>						
+				</div>
+				<?php $reg_sessions = CFS()->get('reg_session_loop'); ?>
+
+				<?php	foreach ($reg_sessions as $reg_session) : ?>
+					<?php	if ( 
+							($reg_session['reg_summer_session'] && $season == "summer") ||
+							(!$reg_session['reg_summer_session'] && $season =="winter") ) : ?>
+						<div class="<?php echo $season; ?> table-row">
+							<span class="camp-number"><?php echo $reg_session['reg_camp_no'];?></span>
+							<span class="date-range"><?php echo $reg_session['reg_date_range'];?></span>
+							<span class="length"><?php echo $reg_session['reg_length'];?></span>
+							<span class="camp-program"><?php echo $reg_session['reg_program'];?></span>
+							<span class="fee"><?php echo $reg_session['reg_fee'];?></span>						
+						</div>
+					<?php endif; ?>
+				<?php endforeach; ?>
+
+				<div class="<?php echo $season; ?> table-footer">
+					<?php if (CFS()->get('warn_' . $season)) : ?>
+						<i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+						<span class="warn-message"><?php echo CFS()->get( 'warn_message' ); ?></span>
+					<?php endif; ?>
+				</div>
+			</div> <!--.season2.table-container-->
+		</div> <!--.season2-registration-table-->
 
 	</main><!-- #main -->
 </div><!-- #primary -->
