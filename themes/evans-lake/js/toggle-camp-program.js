08539e980f31e126f1d0aae8bd52bdadb3928a9f
Victor Salmon <vasalmon@hotmail.com>
2017-06-20 21:30:04 -0700

    Finish toggle-camp-program.js, working tabs

--- themes/evans-lake/js/toggle-camp-program.js
+++ themes/evans-lake/js/toggle-camp-program.js
@@ -1,33 +1,52 @@
 (function($){
-  $('.tab-head.youth-camp-8-12').on('click', function(e){
+  var $youthTabHead = $('.tab-head.youth-camp-8-12'),
+        $youthTabBody = $('.tab-body.youth-camp-8-12'),
+        $juniorTabHead = $('.tab-head.junior-teen-10-14'),
+        $juniorTabBody = $('.tab-body.junior-teen-10-14'),
+        $teenTabHead = $('.tab-head.teen-camp-13-16'),
+        $teenTabBody = $('.tab-body.teen-camp-13-16'),
+        $oakTabHead = $('.tab-head.oak-13-16'),
+        $oakTabBody = $('.tab-body.oak-13-16'),
+        $leaderTabHead = $('.tab-head.leadership-14-16'),
+        $leaderTabBody = $('.tab-body.leadership-14-16')
+        
+  $youthTabHead.on('click', function(e){
     e.preventDefault();
     $('.tab-head.selected').removeClass('selected');
+    $('.tab-body.selected').removeClass('selected');
+    $youthTabHead.addClass('selected');
+    $youthTabBody.addClass('selected');
+  });
 
-    $('.tab-body.junior').addClass('selected');
-  }).addClass('selected');
+  $juniorTabHead.on('click', function(e){
+    e.preventDefault();
+    $('.tab-head.selected').removeClass('selected');
+    $('.tab-body.selected').removeClass('selected');
+    $juniorTabHead.addClass('selected');
+    $juniorTabBody.addClass('selected');
+  });
 
-  // $('.tab-head.youth-camp-8-12').on('click', function(e){
-  //   e.preventDefault();
-  //   $('.tab-head.selected').removeClass('selected');
-  //   $('.tab-body.youth-camp-8-12').addClass('selected');
-  // }).addClass('selected');
+  $teenTabHead.on('click', function(e){
+    e.preventDefault();
+    $('.tab-head.selected').removeClass('selected');
+    $('.tab-body.selected').removeClass('selected');
+    $teenTabHead.addClass('selected');
+    $teenTabBody.addClass('selected');
+  });
 
-  //   $('.tab-head.youth-camp-8-12').on('click', function(e){
-  //   e.preventDefault();
-  //   $('.tab-head.selected').removeClass('selected');
-  //   $('.tab-body.youth-camp-8-12').addClass('selected');
-  // }).addClass('selected');
+  $oakTabHead.on('click', function(e){
+    e.preventDefault();
+    $('.tab-head.selected').removeClass('selected');
+    $('.tab-body.selected').removeClass('selected');
+    $oakTabHead.addClass('selected');
+    $oakTabBody.addClass('selected');
+  });
 
-  //   $('.tab-head.youth-camp-8-12').on('click', function(e){
-  //   e.preventDefault();
-  //   $('.tab-head.selected').removeClass('selected');
-  //   $('.tab-body.youth-camp-8-12').addClass('selected');
-  // }).addClass('selected');
-
-  //   $('.tab-head.youth-camp-8-12').on('click', function(e){
-  //   e.preventDefault();
-  //   $('.tab-head.selected').removeClass('selected');
-  //   $('.tab-body.youth-camp-8-12').addClass('selected');
-  // }).addClass('selected');
-
+  $leaderTabHead.on('click', function(e){
+    e.preventDefault();
+    $('.tab-head.selected').removeClass('selected');
+    $('.tab-body.selected').removeClass('selected');
+    $leaderTabHead.addClass('selected');
+    $leaderTabBody.addClass('selected');
+  });
 })(jQuery);
