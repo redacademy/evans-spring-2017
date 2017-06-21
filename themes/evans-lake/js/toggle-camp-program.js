561089f6ade0fe799ff62a62f2c049cc29baa49c
Victor Salmon <vasalmon@hotmail.com>
2017-06-21 10:56:23 -0700

    Add comments to toggle-camp-program.js

--- themes/evans-lake/js/toggle-camp-program.js
+++ themes/evans-lake/js/toggle-camp-program.js
@@ -1,16 +1,26 @@
 (function($){
 
-  $('.tab-head').on('click', function(event){
-    event.preventDefault();
-    var tabHeadId = "#" + event.target.id,
-        tabBodyId = "#" + event.target.id + "-body", 
-        $tabBodies = $('.tab-body'),
-        $tabHeads = $('.tab-head');
+  if ($('body').hasClass ("page-template-camp-programs")) {
+    // Set Default Tab
+    $('.youth-camp-8-12').addClass('selected'); 
+    
+    // Add Event Handler to all Tabs
+    $('.tab-head').on('click', function(event){
+      event.preventDefault();
+      // Reduce number of jQuery queries using variables
+      var tabHeadId = "#" + event.target.id,
+          tabBodyId = "#" + event.target.id + "-body", 
+          $tabBodies = $('.tab-body'),
+          $tabHeads = $('.tab-head');
 
-    $tabHeads.removeClass('selected');
-    $tabBodies.removeClass('selected');
-    $(tabHeadId).addClass('selected');
-    $(tabBodyId).addClass('selected');  
-  });
+      // Remove any previous selection
+      $tabHeads.removeClass('selected');
+      $tabBodies.removeClass('selected');
+
+      // Selected clicked tab head and body
+      $(tabHeadId).addClass('selected');
+      $(tabBodyId).addClass('selected');  
+    });
+  }
   
 })(jQuery);
