033735556d491747c178fc9274a4cb49595cb590
Victor Salmon <vasalmon@hotmail.com>
2017-06-20 20:16:48 -0700

    Style camp-program

--- themes/evans-lake/js/toggle-camp-program.js
+++ themes/evans-lake/js/toggle-camp-program.js
@@ -1,3 +1,33 @@
-(function () {
-  console.log ("Hello world!");
-});
+(function($){
+  $('.tab-head.youth-camp-8-12').on('click', function(e){
+    e.preventDefault();
+    $('.tab-head.selected').removeClass('selected');
+
+    $('.tab-body.junior').addClass('selected');
+  }).addClass('selected');
+
+  // $('.tab-head.youth-camp-8-12').on('click', function(e){
+  //   e.preventDefault();
+  //   $('.tab-head.selected').removeClass('selected');
+  //   $('.tab-body.youth-camp-8-12').addClass('selected');
+  // }).addClass('selected');
+
+  //   $('.tab-head.youth-camp-8-12').on('click', function(e){
+  //   e.preventDefault();
+  //   $('.tab-head.selected').removeClass('selected');
+  //   $('.tab-body.youth-camp-8-12').addClass('selected');
+  // }).addClass('selected');
+
+  //   $('.tab-head.youth-camp-8-12').on('click', function(e){
+  //   e.preventDefault();
+  //   $('.tab-head.selected').removeClass('selected');
+  //   $('.tab-body.youth-camp-8-12').addClass('selected');
+  // }).addClass('selected');
+
+  //   $('.tab-head.youth-camp-8-12').on('click', function(e){
+  //   e.preventDefault();
+  //   $('.tab-head.selected').removeClass('selected');
+  //   $('.tab-body.youth-camp-8-12').addClass('selected');
+  // }).addClass('selected');
+
+})(jQuery);
