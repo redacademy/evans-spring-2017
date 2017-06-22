1ac7fddb43a59cbca6b556cc67a1d8f6fe4511f4
Victor Salmon <vasalmon@hotmail.com>
2017-06-22 13:51:12 -0700

    Fix indentation

--- themes/evans-lake/home.php
+++ themes/evans-lake/home.php
@@ -24,6 +24,8 @@ get_sidebar(); ?>
 
 	<main id="main" class="site-main" role="main">
 
+
+
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
--- themes/evans-lake/page-orientation-packages.php
+++ themes/evans-lake/page-orientation-packages.php
@@ -28,29 +28,29 @@ get_sidebar(); ?>
 			<?php get_template_part( 'template-parts/content', 'page' ); ?>
 		<?php endwhile; // End of the loop. ?>
 
-<div class="opw-program container">
-  <?php $opw_programs = CFS()->get('opw_loop');?>
-  <?php foreach ($opw_programs as $opw_program) : ?>
-    <h2 class="opw-program title"><?php echo$opw_program['opw_title'];?></h2>
-    <?php $opw_files = $opw_program['opw_file_loop']; ?>
-    <?php foreach ($opw_files as $opw_file) : ?>
-      <a href="<?php echo $opw_file['opw_file_upload'];?>" class="opw-file-upload">
-        <h3 class="opw-file-label"><?php echo $opw_file['opw_file_label']; ?><i class="fa fa-paperclip" aria-hidden="true"></i></h3>
-      </a>
-    <?php endforeach; ?>
-  <?php endforeach; ?>
-</div>
-
-<h2 class="cancel header">Cancellation Policy</h2>
-<div class="cancel container">
-  <?php $policies = CFS()->get( 'cancel_loop', 35 ); ?>
-  <?php foreach ($policies as $policy) : ?>
-    <div class="cancel block">
-      <span class="cancel condition"><?php echo $policy['cancel_condition'];?> </span>
-      <span class="cancel policy"><?php echo $policy['cancel_policy'];?></span>
+    <div class="opw-program container">
+      <?php $opw_programs = CFS()->get('opw_loop');?>
+      <?php foreach ($opw_programs as $opw_program) : ?>
+        <h2 class="opw-program title"><?php echo$opw_program['opw_title'];?></h2>
+        <?php $opw_files = $opw_program['opw_file_loop']; ?>
+        <?php foreach ($opw_files as $opw_file) : ?>
+          <a href="<?php echo $opw_file['opw_file_upload'];?>" class="opw-file-upload">
+            <h3 class="opw-file-label"><?php echo $opw_file['opw_file_label']; ?><i class="fa fa-paperclip" aria-hidden="true"></i></h3>
+          </a>
+        <?php endforeach; ?>
+      <?php endforeach; ?>
     </div>
-    <?php endforeach; ?>
-</div>
+
+    <h2 class="cancel header">Cancellation Policy</h2>
+    <div class="cancel container">
+      <?php $policies = CFS()->get( 'cancel_loop', 35 ); ?>
+      <?php foreach ($policies as $policy) : ?>
+        <div class="cancel block">
+          <span class="cancel condition"><?php echo $policy['cancel_condition'];?> </span>
+          <span class="cancel policy"><?php echo $policy['cancel_policy'];?></span>
+        </div>
+        <?php endforeach; ?>
+    </div>
 
 	</main><!-- #main -->
 
