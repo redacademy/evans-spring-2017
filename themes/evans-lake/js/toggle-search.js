fac17edad8f75d206cf7fb4cc33ca1d5a08206e6
Stanislav Maksarov <maksarov.s@gmail.com>
2017-06-22 10:18:23 -0700

    Create toggle-search function for desktop view

--- themes/evans-lake/js/toggle-search.js
+++ themes/evans-lake/js/toggle-search.js
@@ -1,15 +1,46 @@
-(function($) {
+(function ($) {
 
-  $('.fa-search').click(function(e){
+  $('.fa-search.mobile, .fa-search.desktop').click(function (e) {
     e.preventDefault();
+    
+    if(document.documentElement.clientWidth < 815) {
+
       $('.mobile-search-expanded').show();
       $('.search-field').focus();
       $('.mobile-menu').hide();
+    }
+    else{
+      $('.desktop-search-expanded').show();
+      $('.search-field').focus();
+      $('.main-navigation').hide();
+    }
   });
 
-  $('.content-area').click(function() {
-    $('.mobile-search-expanded').hide();
-    $('.mobile-menu').show();
-  });
+    $('.content-area').click(function() {
+      if(document.documentElement.clientWidth < 815) {
+
+        $('.mobile-search-expanded').hide();
+        $('.mobile-menu').show();
+      }
+      else {
+        $('.desktop-search-expanded').hide();
+        $('.main-navigation').show();
+      }
+    });
+
+    var $window = $(window)
+
+    function resize(){
+      if($window.width() < 815){
+        $('.mobile-menu').show();
+        $('.main-navigation').hide();
+      }
+      if($window.width() > 815){
+        $('.mobile-menu').hide();
+        $('.main-navigation').show();
+      }
+    }
+
+    $window.resize(resize).trigger('resize');
 
 })(jQuery);
