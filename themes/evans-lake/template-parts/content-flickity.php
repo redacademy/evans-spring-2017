cf933240f4231c3c94c9bf6505dc84ee185eafe9
Stanislav Maksarov <maksarov.s@gmail.com>
2017-06-22 23:56:36 -0700

    Add activities carousel on front-page

--- themes/evans-lake/template-parts/content-flickity.php
+++ themes/evans-lake/template-parts/content-flickity.php
@@ -7,7 +7,33 @@
 
 ?>
 
-<h1 class="blog container">News & Events</h1>
+<h1 class="carousel-title">Activities</h1>
+<div class="main-carousel activities container" data-flickity='{ "cellAlign": "left", "contain": true }'>
+
+  <?php
+    $args = array(
+			'post_type'=> 'activity',
+			'post_count'=> 16,
+			'posts_per_page'=> 16);
+  ?>
+  <?php $posts = get_posts( $args ); ?>
+
+  <?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
+  <?php if ( ( CFS()->get ( 'act_img' ) ) != '') : ?>
+    <div class="carousel-cell activities">
+      <div class="activities-thumbnail" style="background-image: linear-gradient(to bottom, rgba(0, 0, 0, .35) 0%, rgba(0, 0, 0, .35) 100%), url('<?php echo CFS()->get('act_img'); ?>'); background-size: cover, cover; background-position: center. center;">
+        <a href="<?php the_permalink(); ?>">
+          <h2 class="activities-title"><?php the_title(); ?></h2>
+        </a>
+      </div>
+    </div>
+  <?php endif; ?>
+  <?php endforeach; ?>
+  <?php wp_reset_postdata(); ?>
+    
+</div>
+
+<h1 class="carousel-title">News & Events</h1>
 <div class="main-carousel container" data-flickity='{ "cellAlign": "left", "contain": true }'>
 
   <?php $args = array( 'posts_per_page' => 3,); ?>
