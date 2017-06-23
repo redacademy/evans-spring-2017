3091236919fc74a37e4b83a72a3ec2a65cb74dde
Victor Salmon <vasalmon@hotmail.com>
2017-06-22 23:02:51 -0700

    Add new page templates for enqueuing checks

---
+++ themes/evans-lake/page-weddings.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying the weddings page.
+ *
+ * @package Evans_Lake_Theme
+ */
+
+get_header(); 
+get_sidebar(); ?>
+
+<div class="hero">
+<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
+	<div class="hero-image" style="background-image: linear-gradient(to bottom, rgba(0, 0, 0, .35) 0%, rgba(0, 0, 0, .35) 100%), url('<?php echo $thumb['0'];?>'); background-size: cover,cover; background-position: center, center;">
+	</div>
+</div>
+<div id="primary" class="content-area container">
+	<div class="sub-navigation">
+		<?php wp_nav_menu( array( 
+			'theme_location' => 'primary', 
+			'menu_id' => 'primary-menu',
+			'submenu' => get_the_title($post->post_parent)
+		) ); ?>
+	</div>
+
+	<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+		<?php endwhile; // End of the loop. ?>
+
+	</main><!-- #main -->
+
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
