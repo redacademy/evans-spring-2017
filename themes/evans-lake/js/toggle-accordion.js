7a26ad289b50ada230af7bc3da9e340945a2b996
Victor Salmon <vasalmon@hotmail.com>
2017-06-22 23:03:44 -0700

    Rename toggle-faq for better semantics

---
+++ themes/evans-lake/js/toggle-accordion.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function($) {
+
+  // Accordion
+  $( function() {
+    var icons={
+      header: 'iconClosed',
+      activeHeader: 'iconOpen'
+    };
+      $( '.accordion' ).accordion({
+        collapsible: true,
+        heightStyle: 'content',
+        icons: icons
+      });
+    } );
+
+    // Tabs
+    $( function() {
+      $( "#tabs" ).tabs();
+    } );
+
+}); 
+
+
