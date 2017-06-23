c4f4389bae30f8ec1253791b65d1400455f7bc89
Victor Salmon <vasalmon@hotmail.com>
2017-06-23 15:10:58 -0700

    Resolve merge conflict markers on repo copy of page-weddings.php

--- themes/evans-lake/page-weddings.php
+++ themes/evans-lake/page-weddings.php
@@ -1,11 +1,7 @@
 <?php
 /**
-<<<<<<< HEAD
  * The template for displaying the weddings page.
-=======
- * The template for displaying wedding page.
->>>>>>> Add food tab to the wedding page
- *
+
  * @package Evans_Lake_Theme
  */
 
@@ -13,13 +9,8 @@ get_header();
 get_sidebar(); ?>
 
 <div class="hero">
-<<<<<<< HEAD
 	<!--Background styled in extras.php/evans_lake_hero_image_update()-->
 	<div class="hero-image">
-=======
-<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
-	<div class="hero-image" style="background-image: linear-gradient(to bottom, rgba(0, 0, 0, .35) 0%, rgba(0, 0, 0, .35) 100%), url('<?php echo $thumb['0'];?>'); background-size: cover,cover; background-position: center, center;">
->>>>>>> Add food tab to the wedding page
 	</div>
 </div>
 <div id="primary" class="content-area container">
@@ -32,15 +23,6 @@ get_sidebar(); ?>
 	</div>
 
 	<main id="main" class="site-main" role="main">
-<<<<<<< HEAD
-
-		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-		<?php endwhile; // End of the loop. ?>
-
-=======
     <button class="orange-button reg-now">Request Information</button> 
 
 		<?php while ( have_posts() ) : the_post(); ?>
@@ -73,13 +55,8 @@ get_sidebar(); ?>
       <?php endforeach; // End of the loop. ?>
       </div><!--.tab-body-container-->
      <?php endwhile; // End of the loop. ?>
->>>>>>> Add food tab to the wedding page
 	</main><!-- #main -->
 
 </div><!-- #primary -->
 
-<<<<<<< HEAD
 <?php get_footer(); ?>
-=======
-<?php get_footer(); ?>
->>>>>>> Add food tab to the wedding page
