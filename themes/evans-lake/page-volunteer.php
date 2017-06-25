8a4cf2f132e823a3c1dc955cfbf550d7c9039947
Victor Salmon <vasalmon@hotmail.com>
2017-06-25 10:33:06 -0700

    Add Volunteer page and content

---
+++ themes/evans-lake/page-volunteer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The template for displaying the volunteering page.
+ *
+ * @package Evans_Lake_Theme
+ */
+
+get_header(); 
+get_sidebar(); ?>
+
+<div class="hero">
+	<!--Background styled in extras.php/evans_lake_hero_image_update()-->
+	<div class="hero-image">
+	</div>
+</div>
+<div id="primary" class="content-area container">
+	<div class="sub-navigation">
+		<?php wp_nav_menu( array( 
+			'theme_location' => 'primary', 
+			'menu_id' => 'primary-menu',
+			'submenu' => get_the_title($post->post_parent)
+		) ); ?>
+	</div>
+
+	<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+		<?php endwhile; // End of the loop. ?>
+		
+    <?php evans_lake_hero_image_update (); ?>
+
+	</main><!-- #main -->
+
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
