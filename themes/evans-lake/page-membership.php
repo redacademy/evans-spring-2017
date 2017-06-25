047376e2645d468707016bb6101f592f501a0bbd
Victor Salmon <vasalmon@hotmail.com>
2017-06-25 08:15:03 -0700

    Add membership page and accurately represent design content

---
+++ themes/evans-lake/page-membership.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The template for displaying the get-involved/membership page.
+ *
+ * @package Evans_Lake_Theme
+ */
+
+get_header(); 
+get_sidebar(); ?>
+
+<div class="hero">
+	<!--Background styled in extras.php/evans_lake_hero_image_update()-->
+	<div class="hero-image">
+	</div>
+</div>
+<div id="primary" class="content-area container">
+	<div class="sub-navigation">
+		<?php wp_nav_menu( array( 
+			'theme_location' => 'primary', 
+			'menu_id' => 'primary-menu',
+			'submenu' => get_the_title($post->post_parent)
+		) ); ?>
+	</div>
+
+	<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+		<?php endwhile; // End of the loop. ?>
+
+  <a href="https://evans-lake-tuck-shop.myshopify.com/collections/featured-items/products/evans-lake-membership">
+    <button class="orange-button">Become a Member</button>
+  </a>
+
+	</main><!-- #main -->
+
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
+
+
+
+
+
