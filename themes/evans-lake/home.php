ec5308afe9cdf5cc495ec750d2be5afadf8fb68e
Victor Salmon <vasalmon@hotmail.com>
2017-06-26 15:35:23 -0700

    Add table output from CFS fields to home.php

--- themes/evans-lake/home.php
+++ themes/evans-lake/home.php
@@ -10,24 +10,54 @@ get_sidebar(); ?>
 
 <div class="hero">
 	<!--Background styled in extras.php/evans_lake_hero_image_update()-->
-	<div class="hero-image">
-	</div>
+	<div class="hero-image"></div>
 </div>
 <div id="primary" class="content-area container">
 	<main id="main" class="site-main" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
-			<div class="single-container box-pop-out">
-				<?php get_template_part( 'template-parts/content', 'single' ); ?>			
-			</div>
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
+	<div class="events-table">	
+		<?php	$events = CFS()->get( 'events', 549 ); ?>
+		<h2>News & Events</h2>
+		<?php foreach ($events as $event) : ?>
+			<div class="table-container box-pop-out">
+				<div class="table-header">
+					<span class="date">Date</span>
+					<span class="event">Event</span>
+					<span class="location">Location</span>
+				</div>
 
-		<?php endwhile; // End of the loop. ?>
+				<div class="table-row <?php	if ($event['event_closed']) { echo "event-closed"; }; ?>">
+					<span class="date">
+						<?php echo $event['event_date']; ?>
+					</span>
+					<span class="event">
+						<?php echo $event['event_name']; ?>
+					</span>
+					<span class="location">
+						<?php	
+						if ($event['event_closed']) { 
+							echo "Closed"; 
+						} else {
+							echo $event['event_location'];
+						}; 
+						?>
+					</span>						
+				</div> <!--table row-->
+			</div> <!--table container-->
+		<?php endforeach; ?>       
+		</div> <!--event container-->
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<div class="single-container box-pop-out">
+					<?php get_template_part( 'template-parts/content', 'single' ); ?>			
+				</div>
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
