e708602202f29d4451f45f8c7ca17bf8f96d73d1
Victor Salmon <vasalmon@hotmail.com>
2017-06-26 16:15:41 -0700

    Style News & Events page and content-single

--- themes/evans-lake/home.php
+++ themes/evans-lake/home.php
@@ -15,51 +15,49 @@ get_sidebar(); ?>
 <div id="primary" class="content-area container">
 	<main id="main" class="site-main" role="main">
 
-	<div class="events-table">	
-		<?php	$events = CFS()->get( 'events', 549 ); ?>
-		<h2>News & Events</h2>
-		<?php foreach ($events as $event) : ?>
+		<div class="events-table">	
+			<?php	$events = CFS()->get( 'events', 549 ); ?>
+			<h2>News & Events</h2>
 			<div class="table-container box-pop-out">
 				<div class="table-header">
 					<span class="date">Date</span>
 					<span class="event">Event</span>
 					<span class="location">Location</span>
 				</div>
-
-				<div class="table-row <?php	if ($event['event_closed']) { echo "event-closed"; }; ?>">
-					<span class="date">
-						<?php echo $event['event_date']; ?>
-					</span>
-					<span class="event">
-						<?php echo $event['event_name']; ?>
-					</span>
-					<span class="location">
-						<?php	
-						if ($event['event_closed']) { 
-							echo "Closed"; 
-						} else {
-							echo $event['event_location'];
-						}; 
-						?>
-					</span>						
-				</div> <!--table row-->
+				<?php foreach ($events as $event) : ?>
+					<div class="table-row <?php	if ($event['event_closed']) { echo "event-closed"; }; ?>">
+						<span class="date">
+							<?php echo $event['event_date']; ?>
+						</span>
+						<span class="event">
+							<?php echo $event['event_name']; ?>
+						</span>
+						<span class="location">
+							<?php	
+							if ($event['event_closed']) { 
+								echo "Closed"; 
+							} else {
+								echo $event['event_location'];
+							}; 
+							?>
+						</span>						
+					</div> <!--table row-->
+				<?php endforeach; ?>   
 			</div> <!--table container-->
-		<?php endforeach; ?>       
 		</div> <!--event container-->
+		<?php while ( have_posts() ) : the_post(); ?>
+			<div class="single-container box-pop-out">
+				<?php get_template_part( 'template-parts/content', 'single' ); ?>			
+			</div>
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			?>
+		<?php endwhile; // End of the loop. ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-				<div class="single-container box-pop-out">
-					<?php get_template_part( 'template-parts/content', 'single' ); ?>			
-				</div>
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-			<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
--- themes/evans-lake/template-parts/content-single.php
+++ themes/evans-lake/template-parts/content-single.php
@@ -4,33 +4,33 @@
  *
  * @package Evans_Lake_Theme
  */
-
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
+		<?php if ( has_post_thumbnail() ) : ?>
+			<div class="hero-image" style="background-image: url ('<?php the_post_thumbnail_url( ); ?>')">
+			</div>
+		<?php else : ?>
+			<div class="hero-image" style="background-image: url (''; ?>')">
+			</div>
+		<?php endif; ?>
+		<a class="entry-title" href="<?php echo get_post_permalink(); ?>">
+			<?php the_title( '<h1>', '</h1>' ); ?>
+		</a>
 		<div class="entry-meta">
 			<?php evans_lake_posted_on(); ?> / <?php evans_lake_comment_count(); ?> / <?php evans_lake_posted_by(); ?>
 		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-				'after'  => '</div>',
-			) );
-		?>
-
-		<?php if ( has_post_thumbnail() ) : ?>
-		<div class="featured-image" style="background-image: url ('<?php the_post_thumbnail_url( ); ?>')">
-			<?php echo "Featured Image Div";?>
-		</div>
-		<?php endif; ?>
+		<?php 
+		the_content();
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+			'after'  => '</div>',
+		) ); ?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
