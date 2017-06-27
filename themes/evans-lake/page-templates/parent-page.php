9393055ddd4ce2176555c69291b54827ca2943a8
Victor Salmon <vasalmon@hotmail.com>
2017-06-27 09:47:24 -0700

    Add parent-page and content-parent template part for the four parent/root pages

---
+++ themes/evans-lake/page-templates/parent-page.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Root Page
+ *
+ * The template for displaying all parent/root pages.
+ *
+ * @package Evans_Lake_Theme
+ */
+
+get_header(); 
+get_sidebar(); ?>
+
+<div class="hero">
+	<!--Background styled in extras.php/evans_lake_hero_image_update()-->
+	<div class="hero-image">
+	</div>
+</div>
+<div id="primary" class="content-area container">
+	<div class="sub-navigation">
+		<?php wp_nav_menu( array( 
+			'theme_location' => 'primary', 
+			'menu_id' => 'primary-menu',
+			'submenu' => get_the_title($post->post_parent)
+		) ); ?>
+	</div>
+
+	<main id="main" class="site-main" role="main">
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content', 'parent' ); ?>
+
+		<?php endwhile; // End of the loop. ?>
+    
+    
+	</main><!-- #main -->
+
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ themes/evans-lake/template-parts/content-parent.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @package Evans_Lake_Theme
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="hero-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<?php evans_lake_breadcrumbs(); ?>
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->
