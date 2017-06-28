93660584ac6a4f8bd492e01d47670f6b2e24d8ae
Victor Salmon <vasalmon@hotmail.com>
2017-06-28 11:53:44 -0700

    Fix parent page content for extended footer

--- themes/evans-lake/template-parts/content-parent.php
+++ themes/evans-lake/template-parts/content-parent.php
@@ -8,7 +8,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="entry-content">
+	<div class="entry-content box-pop-out">
 		<?php the_content(); ?>
 	</div><!-- .entry-content -->
 </article><!-- #post-## -->
