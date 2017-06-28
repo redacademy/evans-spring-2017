ac026fb67d599eb89db3400edeb4177818846d19
Stanislav Maksarov <maksarov.s@gmail.com>
2017-06-28 14:35:51 -0700

    Fix mobile-toggle-search bug on mobile view

--- themes/evans-lake/js/toggle-search.js
+++ themes/evans-lake/js/toggle-search.js
@@ -14,29 +14,30 @@
       }
   });
 
-    $('.site-content').click(function() {
-      if (document.documentElement.clientWidth < 815) {
-        $('.mobile-search-expanded').hide();
-        $('.mobile-menu').show();
-      } else {
-        $('.desktop-search-expanded').hide();
-        $('.main-navigation').show();
-      }
-    });
-
-    var $window = $(window)
-
-    function resize(){
-      if ($window.width() < 815){
-          $('.mobile-menu').show();
-          $('.main-navigation').hide();
-      }
-      if ($window.width() > 815){
-          $('.mobile-menu').hide();
-          $('.main-navigation').show();
-      }
+  $('.site-content').click(function() {
+    if (document.documentElement.clientWidth < 815) {
+      $('.mobile-search-expanded').hide();
+      $('.mobile-menu').show();
+    } else {
+      $('.desktop-search-expanded').hide();
+      $('.main-navigation').show();
     }
+  });
 
+  function resize(){
+    if ($window.width() < 815){
+      $('.mobile-menu').show();
+      $('.main-navigation').hide();
+    }
+    if ($window.width() > 815){
+      $('.mobile-menu').hide();
+      $('.main-navigation').show();
+    }
+  }
+
+  if ($('.desktop-search-expanded, .mobile-search-expanded').length === 0 ) {
+    var $window = $(window);
     $window.resize(resize).trigger('resize');
+  }
 
 })(jQuery);
